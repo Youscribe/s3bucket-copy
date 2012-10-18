0c60daa0dd2e3e08c929e7ba5b4e5ba5de9eebce
Guilhem Lettron <guilhem@lettron.fr>
2012-10-18 15:00:50 +0200

    add command line parsing and options
    This is now a command line software. each option can be found by bucket_copy.py -h
    New options are available :
    	host
    	insecure
    	ordinarycallingformat
    	preserve-acl

--- bucket_copy.py
+++ bucket_copy.py
@@ -2,19 +2,36 @@ import threading
 import datetime
 import time
 import sys
+import argparse
 from threading import Thread
 from multiprocessing.synchronize import BoundedSemaphore
-from boto.s3.connection import S3Connection
+from boto.s3.connection import S3Connection, OrdinaryCallingFormat
 
 AWS_ACCESS_KEY_ID = ""
 AWS_SECRET_ACCESS_KEY = ""
 
-def copy_s3_bucket(SOURCE_BUCKET, DEST_BUCKET, prefix=None, threads=10):
+def s3connect(preserve_acl=False, insecure=False, ordinarycallingformat=False, host=None, debug=None):
+	connection_parameters = {}
+	connection_parameters['aws_access_key_id'] = AWS_ACCESS_KEY_ID
+	connection_parameters['aws_secret_access_key'] = AWS_SECRET_ACCESS_KEY
+	if ordinarycallingformat:
+		connection_parameters['calling_format'] = OrdinaryCallingFormat()
+	if insecure:
+		connection_parameters['is_secure'] = False
+	if host:
+		connection_parameters['host'] = host
+	c = S3Connection(**connection_parameters)
+	if debug:
+		c.debug = debug
+	return c
+	
+
+def copy_s3_bucket(SOURCE_BUCKET, DEST_BUCKET, prefix=None, threads=10, preserve_acl=False, insecure=False, ordinarycallingformat=False, host=None, debug=None):
 	"""
 	Example usage: copy_s3_bucket(SOURCE_BUCKET='my-source-bucket', DEST_BUCKET='my-destination-bucket', prefix='parent/child/dir/', threads=20)
 	"""
 	# Init s3
-	conn = S3Connection(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+	conn = s3connect(preserve_acl,insecure,ordinarycallingformat,host,debug)
 	bucket = conn.get_bucket(SOURCE_BUCKET)
 	dest_bucket = conn.get_bucket(DEST_BUCKET)
 
@@ -29,7 +46,7 @@ def copy_s3_bucket(SOURCE_BUCKET, DEST_BUCKET, prefix=None, threads=10):
 			self.status = False
 		def run(self):
 			# We must create new bucket instances for each thread, passing the key is not threadsafe
-			thread_conn = S3Connection(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+			thread_conn = conn = s3connect(preserve_acl,insecure,ordinarycallingformat,host,debug)
 			thread_bucket = conn.get_bucket(SOURCE_BUCKET)
 			thread_dest_bucket = conn.get_bucket(DEST_BUCKET)
 			thread_key = thread_bucket.get_key(self.key_name)
@@ -77,3 +94,66 @@ def copy_s3_bucket(SOURCE_BUCKET, DEST_BUCKET, prefix=None, threads=10):
 		print "%s : Status Output: %s" % (datetime.datetime.now(), key_copy_thread.status)
 
 	print "%s : Complete : %s Total Keys Requested" % (datetime.datetime.now(), total_keys)
+
+def main():
+	parser = argparse.ArgumentParser(
+		formatter_class=argparse.RawDescriptionHelpFormatter,
+		usage='%(prog)s [OPTIONS] SOURCE DESTINATION',
+		add_help=False
+		)
+	parser.add_argument(
+		'-a', '--access-key', metavar='KEY', dest='access_key',
+		help='Your Access Key ID'
+		)
+	parser.add_argument(
+		'-s', '--secret-key', metavar='SECRET', dest='secret_access_key',
+		help='Your Secret Key.'
+		)
+	parser.add_argument(
+		'--insecure', action='store_true',
+		help='Connect without SSL'
+		)
+	parser.add_argument(
+		'--ordinarycallingformat' , action='store_true',
+		help='Connect with the old connection type ' + \
+		'(no subdomain and Bucket upper case).'
+		)
+	parser.add_argument(
+		'--host', metavar='HOST', default='s3.amazonaws.com',
+		help='Specify a specific S3 endpoint to connect to via boto\'s ' + \
+		'"host" connection argument (S3 only).'
+		)
+	parser.add_argument(
+		'--threads', default='10', type=int,
+		help='number of threads'
+		)
+	parser.add_argument(
+		'-p', '--preserve-acl', dest='preserve_acl', action='store_true',
+		help='Copy the ACL from the source key to the destination key'
+		)
+	parser.add_argument(
+		'-d', '--debug', metavar='LEVEL', choices=[0, 1, 2], default=0,
+		type=int,
+		help='Level 0 means no debug output (default), 1 means normal ' + \
+			'debug output from boto, and 2 means boto debug output plus ' + \
+			'request/response output from httplib.'
+		)
+		
+
+	parser.add_argument('SOURCE', help=argparse.SUPPRESS)
+	parser.add_argument('DESTINATION', help=argparse.SUPPRESS)
+
+	try:
+		args = parser.parse_args()
+	except argparse.ArgumentTypeError:
+		pass
+
+	global AWS_ACCESS_KEY_ID
+	global AWS_SECRET_ACCESS_KEY
+	AWS_ACCESS_KEY_ID = args.access_key
+	AWS_SECRET_ACCESS_KEY = args.secret_access_key
+
+	copy_s3_bucket(args.SOURCE, args.DESTINATION, threads=args.threads, insecure=args.insecure, ordinarycallingformat=args.ordinarycallingformat, preserve_acl=args.preserve_acl, host=args.host, debug=args.debug)
+
+if __name__ == "__main__":
+    main()
